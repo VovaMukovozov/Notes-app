7d4706bb4a514974419220b1c5300a68e8629b97
vova_mukovozov <vova.mukovozov@sergata.com>
2016-03-26 13:28:11 +0300

    added angular app

---
+++ src/app/index.module.js
@@ -0,0 +1,7 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('notesApp', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ui.router', 'ui.bootstrap', 'toastr']);
+
+})();
